af480c2b119e4a9ef2a73c62edaefe17cdd07461
unknown <golla.kumar.sravan@gmail.com>
2023-07-15 13:59:10 -0400

    psycopg2 changes to connect to postgresql DB

---
+++ app/app/calc.py
@@ -0,0 +1,20 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """ Add x and y  and returns result."""
+    return x + y
+
+def substract(x,y):
+    """Substracts the two numbers"""
+    return x-y
+
+def do_something(x):
+    if x==0:
+        print(0)
+    elif x==1:
+        print(1)
+
+    
---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Tests the calc module """
+    def test_add_two_numbers(self):
+        """ tests the adding of two numbers"""
+        res = calc.add(5,4)
+
+        self.assertEqual(res,9)
+
+    def test_substract_numbers(self):
+        """tests the substract numbers"""
+        res = calc.substract(2,1)
+        self.assertEqual(res,1)
+
